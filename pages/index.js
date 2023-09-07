8bd68a3b652eae407f5fae816c9c82d83d23f937
ranielletuazon <143058053+ranielletuazon@users.noreply.github.com>
2023-09-07 17:25:09 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,19 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>I am a University of the Assumption student!</p>
+
+        <p>
+        Hello, my name is Ranielle Adriane Tuazon, some and everybody calls me Rain. I’m an
+        incoming 3 rd Year Student, nice to meet you po sir!
+        </p>
+        <p>
+        My hobbies are mostly an average person what he should be like plays computer games,
+        going outside and do basketball, and a simple vocalist from a band. I mostly listen to OPM
+        and I do really like pop, or anything what people likes about a certain taste in music.
+        Syempre my favorite food is what my grandmother always cooks whenever I come visit her
+        and its “Giniling” with potatoes and carrots in it.
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
