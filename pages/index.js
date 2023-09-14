343bcc97b2b2982532c639f8b88e96d0813345f6
ranielletuazon <143058053+ranielletuazon@users.noreply.github.com>
2023-09-14 11:00:16 +0800

    index.js

--- pages/index.js
+++ pages/index.js
@@ -9,18 +9,23 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I am a University of the Assumption student!</p>
+        <p>
+          10 Things That Require Zero Talent
+        </p>
 
         <p>
-        Hello, my name is Ranielle Adriane Tuazon, some and everybody calls me Rain. I’m an
-        incoming 3 rd Year Student, nice to meet you po sir!
-        </p>
-        <p>
-        My hobbies are mostly an average person what he should be like plays computer games,
-        going outside and do basketball, and a simple vocalist from a band. I mostly listen to OPM
-        and I do really like pop, or anything what people likes about a certain taste in music.
-        Syempre my favorite food is what my grandmother always cooks whenever I come visit her
-        and its “Giniling” with potatoes and carrots in it.
+          <ul>
+            <li>1. Being On Time</li>
+            <li>2. Making An Effort</li>
+            <li>3. Being High Energy</li>
+            <li>4. Having A Positive Attitude</li>
+            <li>5. Being Passionate</li>
+            <li>6. Using Good Body Language</li>
+            <li>7. Being Coachable</li>
+            <li>8. Doing A Little Extra</li>
+            <li>9. Being Prepared</li>
+            <li>10. Having A Strong Work Ethic</li>
+          </ul>
         </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
